c494e2c6457b6e212e49e9cd53328cea62295182
unknown <ayoub1benmansour@gmail.com>
2026-02-09 20:43:25 +0100

    Styled the login, register, profile, home pages

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,11 @@
 import { UserService } from './../../core/services/user.service';
 import { Component } from '@angular/core';
-import { Router, RouterLink } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
