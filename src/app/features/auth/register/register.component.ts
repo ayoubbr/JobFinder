4c625faa95b299d26b2cbecd86b35230fdaa11f3
unknown <ayoub1benmansour@gmail.com>
2026-02-09 11:21:44 +0100

    Header added for navigation, Register form added

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]]
+    });
+  }
+
+  onSubmit() {
+    this.registerForm.valid ? console.log(this.registerForm.value) : "no data in";
+  }
 
 }
